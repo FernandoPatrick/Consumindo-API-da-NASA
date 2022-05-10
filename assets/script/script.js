0175fc8b7e8ea84d06291224405db9c3b96d23f4
Fernando Patrick <fernando_lopes27@hotmail.com>
2022-05-10 17:25:04 -0400

    fisrt commit

---
+++ assets/script/script.js
@@ -0,0 +1,40 @@
+$("#enviar").click(function () {
+  let fadd = $("#data").val();
+  $.ajax({
+    url: `https://api.nasa.gov/planetary/apod?api_key=jc9sUQZK5upx2PqEy4xX3jKCmGYnNN40qRMACt1U&date=${fadd}`,
+    type: "GET",
+    success: function (data) {
+      console.log(data);
+      $("body").css("height", "");
+      $("#titulo").text(data.title);
+      $("#imagem").css("display", "none");
+      $("#imagem-apod").attr("src", data.url);
+      $("#coment").text(data.explanation);
+    },
+  });
+});
+
+$("#ftHoje").click(function () {
+  $.ajax({
+    url: `https://api.nasa.gov/planetary/apod?api_key=jc9sUQZK5upx2PqEy4xX3jKCmGYnNN40qRMACt1U&`,
+    type: "GET",
+    success: function (data) {
+      console.log(data);
+      $("body").css("height", "");
+      $("#titulo").text(data.title);
+      $("#imagem").css("display", "none");
+      $("#imagem-apod").attr("src", data.url);
+      $("#coment").text(data.explanation);
+    },
+  });
+});
+
+// function googleTranslateElementInit() {
+//   new google.translate.TranslateElement(
+//     {
+//       pageLanguage: "en",
+//       layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+//     },
+//     "translate"
+//   );
+// }
